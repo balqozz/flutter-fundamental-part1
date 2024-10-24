e55e3c484ed93e1147a5e85d4efa41d490f5feac
balqozz <145304918+balqozz@users.noreply.github.com>
2024-10-25 02:56:19 +0700

    Prac4

--- android/app/src/main/kotlin/com/example/hello_world1/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/hello_world1/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.hello_world1
+package com.example.hello_world
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+// import 'package:hello_world/basic_widgets/text_widget.dart';
+import 'package:hello_world/basic_widgets/image_widget.dart';
 
 void main() {
   runApp(const MyApp());
@@ -105,9 +107,10 @@ class _MyHomePageState extends State<MyHomePage> {
           // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
+            // const Text(
+            // const MyTextWidget(),
+            const MyImageWidget(),
+
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
@@ -123,3 +126,5 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+
+
